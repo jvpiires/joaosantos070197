66d91373a774f639124605eef1ea57ee40c6401b
João Victor Pires Dos Santos <joaopires@sad.infovia-mt>
2026-01-19 14:40:20 -0400

    build: inicializando o projeto backend com Spring Boot

---
+++ backend/src/main/java/br/gov/mt/seplag/sgd/SgdBackendApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.sgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgdBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/gov/mt/seplag/sgd/SgdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.sgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
