be7f929b7a9425722c2f70c77eb1ded2d17ced1c
jvpiires <jv.piires@outlook.com>
2026-01-28 14:30:38 -0400

    feat: adiciona inserção de dados para artistas e álbuns

---
+++ backend/src/main/resources/db/migration/V3__insert_table_artists_albuns.sql
@@ -0,0 +1,25 @@
+INSERT INTO artists (name, created_at, updated_at) VALUES
+                   ('Adele', NOW(), NOW()),
+                   ('The Beatles', NOW(), NOW()),
+                   ('Taylor Swift', NOW(), NOW()),
+                   ('Michael Jackson', NOW(), NOW()),
+                   ('Beyoncé', NOW(), NOW()),
+                   ('Elvis Presley', NOW(), NOW()),
+                   ('Madonna', NOW(), NOW()),
+                   ('Bob Dylan', NOW(), NOW()),
+                   ('Queen', NOW(), NOW()),
+                   ('Pink Floyd', NOW(), NOW()),
+                   ('Bruno Mars', NOW(), NOW());
+
+INSERT INTO albums (title, artist_id, created_at, updated_at) VALUES
+                    ('21', 1, NOW(), NOW()),
+                    ('Abbey Road', 2, NOW(), NOW()),
+                    ('1989', 3, NOW(), NOW()),
+                    ('Thriller', 4, NOW(), NOW()),
+                    ('Lemonade', 5, NOW(), NOW()),
+                    ('Elvis Presley', 6, NOW(), NOW()),
+                    ('Like a Virgin', 7, NOW(), NOW()),
+                    ('Highway 61 Revisited', 8, NOW(), NOW()),
+                    ('A Night at the Opera', 9, NOW(), NOW()),
+                    ('The Dark Side of the Moon', 10, NOW(), NOW()),
+                    ('Do-Wops & Hooligans', 11, NOW(), NOW());
